023546eefb14dd056c9212bb0c19b96c32fd151d
__nkO <Carmady@host-155-246-199-90.dhcp.stevens-tech.edu>
2015-10-13 11:07:26 -0400

    Common Item Collider Event
    Uploaded Unity JS Script for Collision Trigger Event

---
+++ Common_Items/Collide_test.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var text_obj : GameObject;
+
+
+text_obj.GetComponent.<SpriteRenderer>().enabled = false;
+
+function OnTriggerEnter2D(other : Collider2D){
+	if(other.gameObject.tag == "Player"){
+		text_obj.GetComponent.<SpriteRenderer>().enabled = true;
+	}
+}
+
+function OnTriggerExit2D(other : Collider2D){
+	if(other.gameObject.tag == "Player"){
+		text_obj.GetComponent.<SpriteRenderer>().enabled = false;
+	}
+}
