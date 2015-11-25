d59043c43f2617445fa2a02a8f20cc0ca80b6516
adumanda <adumanda@stevens.edu>
2015-11-24 21:06:28 -0500

    Create addPoints.js

---
+++ Inventory/adumanda/addPoints.js
@@ -0,0 +1,25 @@
+//Show and update points using GUI Text
+#pragma strict
+private var score: int = 0;
+var guiScore : GUIText;
+
+function Start () {
+	guiScore.text = "Score: 0";
+}
+
+
+function Update () {
+
+}
+
+function OnTriggerEnter2D (other: Collider2D)
+{
+	if (other.gameObject.tag =="Player")
+	{
+        Destroy(gameObject);
+        score += 10;
+        guiScore.text = "Score: " + score;
+        print ("Coin Collected!");
+        
+	}
+}
